e0f1308c19b62de40df29afe08655e1877571bc1
Ilya Levchenko <reasonablequestionn1@gmail.com>
2020-11-03 19:25:34 +0300

    Add project files.

---
+++ Models/Dictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnglishApi.Models
+{
+    public class Dictionary
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string SecretName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Name { get; set; }
+    }
+}
---
+++ Models/DictionaryWord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnglishApi.Models
+{
+    public class DictionaryWord
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid DictionaryId { get; set; }
+
+        [ForeignKey("DictionaryId")]
+        public Dictionary Dictionary { get; set; }
+
+        [Required]
+        public Guid WordId { get; set; }
+
+        [ForeignKey("WordId")]
+        public Word Word { get; set; }
+    }
+}
